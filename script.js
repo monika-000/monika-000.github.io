7d64b5301ec57331101090243378efc0ccc81f6d
Monika <mprzybylska601@gmail.com>
2023-06-07 17:16:05 +1000

    Updated JS

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 let buttons = document.getElementsByClassName("navBar-button");
 let cards = document.getElementsByClassName("navbar-card");
 
-displayCard();
-
-function displayCard(){
-    for(let i = 0; i < buttons.length; i++){
-        buttons[i].addEventListener('click', () => {
-            cards[i].style.display = "block"; 
-        });
-        cards[i].style.display = "none";
-    }   
+ function displayCard(sectionName){
+    for(let i = 0; i< buttons.length; i++){
+           cards[i].style.display = "none"
+    }
+   document.getElementById(sectionName).style.display ="block";
 }
