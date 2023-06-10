db1ca924dec74f0c0010c665293e5c09697d0f78
Monika <mprzybylska601@gmail.com>
2023-06-10 15:51:55 +1000

    Modal

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 function displayCard(sectionName){
     let cards = document.getElementsByClassName("navbar-card");
+    //let modal = document.getElementsByClassName(".modal");
     for(let i = 0; i< cards.length; i++){
-           cards[i].style.display = "none"
+    //for(let i = 0; i< modal.length; i++){
+        cards[i].style.display = "none"
+        //modal[i].style.display = "none"
     }
    document.getElementById(sectionName).style.display ="block";
 }
