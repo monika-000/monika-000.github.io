bb6eb5e1be7a40f91e90d950f210088c51580daf
Monika <mprzybylska601@gmail.com>
2023-06-13 19:40:11 +1000

    Fixed h1 and nav card layout

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 function displayCard(sectionName){
     let cards = document.getElementsByClassName("navbar-card");
+    let welocmeMessage = document.getElementById("welcomeMessage");
     //let modal = document.getElementsByClassName(".modal");
     for(let i = 0; i< cards.length; i++){
     //for(let i = 0; i< modal.length; i++){
         cards[i].style.display = "none"
+        welocmeMessage.style.display = "block"
         //modal[i].style.display = "none"
     }
-   document.getElementById(sectionName).style.display ="block";
+    welocmeMessage.style.display = "none"
+    document.getElementById(sectionName).style.display ="block";
 }
